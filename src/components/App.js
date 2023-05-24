6c689d9687bf8b94b5a3d3f7551f3b93f398b8c7
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-05-24 21:54:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
-
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import MobileDetails from "./MobileDetails";
+import AdminPage from "./AdminPage";
+import MobileList from "./MobileList";
 import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <MobileList />
+          </Route>
+          <Route path="/products/:id">
+            <MobileDetails />
+          </Route>
+          <Route path="/admin">
+            <AdminPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
